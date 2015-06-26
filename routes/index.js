1ea5c8b586fc575b912126153f7f416e27f4651d
Pedro Reyes <pedro-reyes@hotmail.es>
2015-06-26 19:59:04 +0200

    Tarea P2P Modulo 6

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'autor' });
+});
+
+
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
 
